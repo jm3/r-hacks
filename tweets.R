5383699c0bb7601e442297cd34bbcb82e1f1cf05
John Manoogian III <jm3@jm3.net>
2012-05-20 12:22:14 -0700

    don't use transform because it doesn't seem to intuit scope correctly

--- tweets.R
+++ tweets.R
@@ -10,53 +10,54 @@ library(stringr) # mine text
 
 favs <- read.csv("./favs.csv")
 
-# clean up the data
-favs <- transform(favs,
-  # drop unneeded column
-  avatar <- NULL,
+attach(favs)
+# tweet IDs are opaque identifiers; we don't do math on them; thus factors
+favs$tweet_id <-factor(tweet_id, order=T)
 
-  # tweet IDs are opaque identifiers; we don't do math on them; thus factors
-  tweet_id <-factor(tweet_id, order=T),
+# strings not factors
+favs$tweet <- as.character(tweet)
+favs$name <- factor(paste(name, " (@", screen_name, ")", sep=""))
 
-  # strings not factors
-  tweet <- as.character(tweet),
-  name <- as.character(name),
+# drop unneeded columns after merging
+favs$avatar <- NULL
+favs$screen_name <- NULL
 
-  # coerce to POSIXct
-  tweet_date <- mdy_hms(tweet_date)
-
-  # derive some values
-  link_pos     = str_locate(tweet,"http"),
-  RT_pos       = str_locate(tweet,"RT"),
-  mention_pos  = str_locate(tweet,"@"),
-  length       = str_length(tweet),
-)
+# coerce to POSIXct
+favs$date <- mdy_hms(date)
+favs$link_pos     = str_locate(tweet,"http")
+favs$RT_pos       = str_locate(tweet,"RT")
+favs$mention_pos  = str_locate(tweet,"@")
+favs$length       = str_length(tweet)
+detach(favs)
 
 tweets <- read.csv("./tweets.csv")
-tweets <- transform(tweets,
-  # merge date + time
-  date <- ymd_hm(paste(date,time))
-  # poor man's version: tweets$date = as.Date(paste(tweets$date,tweets$time))
+attach(tweets)
+# merge date + time
+tweets$date <- ymd_hm(paste(date,time))
+# poor man's version: tweets$date = as.Date(paste(tweets$date,tweets$time))
 
-  # drop unneeded columns
-  time      <- NULL
-  length    <- NULL
-  RT_pos    <- NULL
-  link_pos  <- NULL
+# drop unneeded columns
+tweets$time      <- NULL
+tweets$length    <- NULL
+tweets$RT_pos    <- NULL
+tweets$link_pos  <- NULL
 
-  # derive some stats
-  link_pos     = str_locate(tweet,"http")
-  RT_pos       = str_locate(tweet,"RT")
-  mention_pos  = str_locate(tweet,"@")
-  length       = str_length(tweet)
-)
+# strings not factors
+tweets$tweet <- as.character(tweet)
+
+# derive some stats
+tweets$link_pos     = str_locate(tweet,"http")
+tweets$RT_pos       = str_locate(tweet,"RT")
+tweets$mention_pos  = str_locate(tweet,"@")
+tweets$length       = str_length(tweet)
+detach(tweets)
 
 # plots some hotness
-fit <- lm(favs$length ~ favs$tweet_date)
-plot(favs$tweet_date,favs$length); abline(fit)
+fit <- lm(favs$length ~ favs$date)
+plot(favs$date,favs$length); abline(fit)
 plot(density(favs$length))
 plot(density(tweets$length))
-fit <- lm(favs$length ~ favs$tweet_date)
+fit <- lm(favs$length ~ favs$date)
 plot(fit)
 rm(fit)
 
