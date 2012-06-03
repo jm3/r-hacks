f5ee52592e96ed76b7cfe951eacffbe6ed6652b6
John Manoogian III <jm3@jm3.net>
2012-06-02 17:55:51 -0700

    clean up code, use local tweets (don't fetch over the network), move distr funcs into separate file, rename plots to flatline notation, add more stop names

--- tweets.R
+++ tweets.R
@@ -9,10 +9,9 @@ library(timeSeries) # work with TS data
 library(tm) # mine text
 library(twitteR) # use the Twitter API 
 library(wordcloud) # generate visual word clouds
+source('distributions.R')
 source('multiplot.R')
 
-#Check if the package is installed
-#is.installed('MASS') 
 favs <- read.csv("./data/favs.csv")
 
 attach(favs)
@@ -56,79 +55,73 @@ tweets$mention_pos  = str_locate(tweet,"@")
 tweets$length       = str_length(tweet)
 detach(tweets)
 
-dtnorm0 <- function(x, mean = 0, sd = 1, log = FALSE) {
-  dtnorm(x, mean=mean, sd=sd, lower=1, upper=140)
-}
-
-#Attempting to use maximum likelihood estimation to fit some data to a truncated normal distribution
+# try using maximum likelihood estimation to fit some data to a truncated normal dist.
 fitdistr(favs$length, dtnorm0, start = list(mean = 0, sd = 1))
+favs_samples   <- twitter_dist(favs$length)
+tweets_samples <- twitter_dist(tweets$length)
 
-#Individual density plots drawn randomly from a truncated normal (rtnorm)
-twitter_dist <- function( vec, num_runs=10000 ) {
-  data.frame(dist=rtnorm(num_runs, mean=mean(vec), sd=sd(vec), lower=1, upper=140))
-}
-favs_samples   = twitter_dist(favs$length)
-tweets_samples = twitter_dist(tweets$length)
+# overlay two density plots
+num_obs <- 10000
+series <- c(array("Favorites", num_obs), array("All Tweets", num_obs))
+merged_data <- data.frame( series=series, data=c(favs_samples$dist, tweets_samples$dist))
+tweet_vs_fav_density_plot <- ggplot(merged_data, aes(x=merged_data$data, fill=series)) + geom_density(alpha = 0.8)
+rm(num_obs, plot, favs_samples, tweets_samples)
 
-plot <- ggplot(favs_samples, aes(x=dist))
-plot + geom_density(alpha = 0.8, fill="#498376", colour="#498376")
+# plot the basic time-series data
+fav_length_plot <- ggplot(favs, aes(date, length)) + geom_line() + xlab("") + ylab("Tweet Lengths")
 
-#Overlay two density plots
-num_obs = 10000
-series=c(array("Favorites", num_obs), array("All Tweets", num_obs))
-merged_data = data.frame( series=series, data=c(favs_samples$dist, tweets_samples$dist))
-densityTweetsVFavsPlots <- ggplot(merged_data, aes(x=merged_data$data, fill=series)) + geom_density(alpha = 0.8)
-rm(merged_data, num_obs, plot, favs_samples, tweets_samples)
+favs_ts <- timeSeries(favs$length, favs$date, units = "tweet_lengths")
 
-#Plotting a general, time-series data
-tweetLengthPlot=ggplot(favs, aes(date, length)) + geom_line() + xlab("") + ylab("Tweet Lengths")
-
-#Create a time-series object from the data
-favs_ts = timeSeries(favs$length, favs$date, units = "tweet_lengths")
-
-#Perform a weekly aggregation using our timeSeries object
+# perform a weekly aggregation using our timeSeries object
 favs_by_week <- timeSequence(from = start(favs_ts),  to = end(favs_ts), by = "week")
 
-weekly_means = aggregate(favs_ts, favs_by_week, mean) #mean = aggregation mechanism, can use a UDF
-weeks = ymd_hms(rownames(weekly_means))
-graphable_df = data.frame(date=weeks, mean_tweet_length=weekly_means$tweet_lengths)
-AvgTweetLengthPlot=ggplot(graphable_df, aes(date,mean_tweet_length)) + geom_line() + xlab("") + ylab("Mean Length of Favorited Tweet")
+weekly_means <- aggregate(favs_ts, favs_by_week, mean) #mean = aggregation mechanism, can use a UDF
+weeks <- ymd_hms(rownames(weekly_means))
+graphable_df <- data.frame(date=weeks, mean_tweet_length=weekly_means$tweet_lengths)
+mean_tweet_length_plot <- ggplot(graphable_df, aes(date,mean_tweet_length)) + geom_line() + xlab("") + ylab("Mean Length of Favorited Tweet")
 rm(graphable_df)
 
-# Graph number of tweets favorited per week (count #observations in timeseries)
-weekly_counts = aggregate(favs_ts, favs_by_week, FUN=function(x) { length(x) })
-graphable_df = data.frame(date=weeks, tweets_per_week=weekly_counts$tweet_lengths)
-tweetsPerWeekPlot=ggplot(graphable_df, aes(date,tweets_per_week)) + geom_line() + xlab("") + ylab("Number of Tweets Favorited per Week")
+# graph num tweets favorited per week (count #observations in timeseries)
+weekly_counts <- aggregate(favs_ts, favs_by_week, FUN=function(x) { length(x) })
+graphable_df <- data.frame(date=weeks, tweets_per_week=weekly_counts$tweet_lengths)
+tweets_per_week_plot <- ggplot(graphable_df, aes(date,tweets_per_week)) + geom_line() + xlab("") + ylab("Number of Tweets Favorited per Week")
 rm(graphable_df, series)
 
-multiplot(tweetsPerWeekPlot, AvgTweetLengthPlot, tweetLengthPlot, densityTweetsVFavsPlots + opts(legend.position="bottom"), cols=2)
+multiplot(tweets_per_week_plot, mean_tweet_length_plot, fav_length_plot, tweet_vs_fav_density_plot + opts(legend.position="bottom"), cols=2)
 
-recent_tweets <- userTimeline("jm3", n=200)
-recent_tweets[1:3]
-recent_tweets <- do.call("rbind", lapply(recent_tweets, as.data.frame)) # slick; look ma no loops
-dim(recent_tweets)
+##########################################################################################
+# fetch recent tweets over the network via the Twitter API
+# recent_tweets <- userTimeline("jm3", n=200)
+# recent_tweets[1:3]
+# recent_tweets <- do.call("rbind", lapply(recent_tweets, as.data.frame)) # slick; look ma no loops
+# dim(recent_tweets)
 
-tweet_text <- recent_tweets$text
-tweet_corpus <- Corpus(VectorSource(tweet_text))
+# or just use the tweets we've already loaded
+tweet_text <- tweets$tweet
+tweet_corpus <- Corpus(VectorSource(as.vector(tweet_text, mode="character")))
 
 # strip non-ASCII chars because TM can't handle them
-tweet_corpus <-tm_map(tweet_corpus, function(x) iconv(enc2utf8(x), sub = "byte"))
+tweet_corpus <- tm_map(tweet_corpus, function(x) iconv(enc2utf8(x), sub = "byte"))
 tweet_corpus <- tm_map(tweet_corpus, tolower)
 tweet_corpus <- tm_map(tweet_corpus, removePunctuation)
 tweet_corpus <- tm_map(tweet_corpus, removeNumbers)
 
 # exclude some more common stopwords i say a lot
-tweet_stopwords <- c(stopwords('english'), "via", "rt", 
+tweet_stopwords <- c(stopwords('english'), "via", "rt", "im", "youre", "theres"
   "joroan", "jm", "stdoyle", "cstoller", "vnaylon", "jeffheuer", "ayn", "buzz", "jamiew", 
   "peterhoneyman", "bloggerton", "proofapi", "proofads", "bmorrissey", "markjardine", 
   "rrwhite", "timhaines", "jenspec", "adampritzker", "sugarhousebar", "mattbinkowski", 
   "sferik", "tapbotpaul", "daksis", "jschox", "notoriouskrd", "janchip", "jonelvekrog",
   "joshsternberg", "griffitherin", "lmorchard", "pheezy", "sheynkman", "dugsong", "timoni",
-  "konstantinhaase", "trafnar", "farbood", "kmaverick", "tiboutoo", "aidaan", "ahalvor"
+  "konstantinhaase", "trafnar", "farbood", "kmaverick", "tiboutoo", "aidaan", "ahalvor",
+  "lauardelean", "takeitez", "umairh", "kosta", "forzablitz", "dianakimball", "mikenowland",
+  "krogisms", "nanaenay", "jackpine", "seekoh", "rands", "mike", "kevin", "abruehl", "alanlwhite",
+  "alexchoi", "carinnatarvin", "david", "dcurtis", "essl", "carinnatarvin", "michael", "amadad",
+  "formalplay", "samaman", "markromanek", "twilio", "cdixon", "companypolicy"
 )
 tweet_corpus <- tm_map(tweet_corpus, removeWords, tweet_stopwords)
-dtm <- TermDocumentMatrix(tweet_corpus, control = list(minWordLength = 1))
-findFreqTerms(dtm, lowfreq=10)
+dtm <- TermDocumentMatrix(tweet_corpus, control = list(minWordLength = 2))
+findFreqTerms(dtm, lowfreq=4)
 terms_matrix <- as.matrix(dtm)
 
 # calculate the frequency of words
@@ -141,7 +134,7 @@ terms[key_to_rename] <- "jm3"
 rm(key_to_rename)
 
 clean_sorted_terms_matrix <- data.frame(word=terms, freq=sorted_vector_terms)
-wordcloud(clean_sorted_terms_matrix$word, clean_sorted_terms_matrix$freq, min.freq=3)
+wordcloud(clean_sorted_terms_matrix$word, clean_sorted_terms_matrix$freq, min.freq=5)
 rm(k, clean_sorted_terms_matrix, dtm, sorted_vector_terms, terms, terms_matrix, tweet_stopwords)
 
 # working with http://www.rdatamining.com/examples/text-mining +
